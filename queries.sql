4382e583a29d908f168aa4b08733b0a538000c2d
Jarviniazh <zhaojwdkj@gmail.com>
2022-02-09 17:09:04 -0500

    add three lists

--- queries.sql
+++ queries.sql
@@ -86,3 +86,68 @@ left join dept_emp as de
 on ce.emp_no = de.emp_no
 group by de.dept_no
 order by de.dept_no;
+
+-- List 1: Employee Information
+SELECT * FROM salaries;
+
+--sort that column in descending order
+SELECT * FROM salaries
+order by to_date desc;
+
+--new table to store all date
+select emp_no, first_name, last_name, gender
+into emp_info
+from employees
+where (birth_date between '1952-01-01' and '1955-12-31')
+	and (hire_date between '1985-01-01' and '1988-12-31');
+
+drop table emp_info;
+
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+into emp_info
+from employees as e
+	inner join salaries as s
+		on e.emp_no = s.emp_no
+	inner join dept_emp as de
+		on e.emp_no = de.emp_no
+	where (e.birth_date between '1952-01-01' and '1955-12-31')
+		and (e.hire_date between '1985-01-01' and '1988-12-31')
+		and (de.to_date = '9999-01-01');
+
+-- List 2: Management
+-- List of managers per department
+select dm.dept_no,
+	dm.emp_no,
+	dm.from_date,
+	dm.to_date,
+	d.dept_name,
+	ce.first_name,
+	ce.last_name
+into manager_info
+from dept_manager as dm
+	inner join departments as d
+		on dm.dept_no = d.dept_no
+	inner join current_emp as ce
+		on dm.emp_no = ce.emp_no;
+
+select * from manager_info;
+
+
+-- List 3: Department Retirees
+select ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+into dept_info
+from current_emp as ce
+	inner join dept_emp as de
+		on ce.emp_no = de.emp_no
+	inner join departments as d
+		on de.dept_no = d.dept_no;
+
+select * from dept_info;
